0098da43c5f8e3425cfa3cb43e56154dba794910
huseyinb <huseyinsbalik@gmail.com>
2026-08-08 00:23:57 +0300

    Temp root files deleted.

---
+++ JadooTravel/Entities/Category.cs
@@ -0,0 +1,10 @@
+namespace JadooTravel.Entities;
+
+public class Category
+{
+    public string CategoryId { get; set; }
+    public string IconUrl { get; set; }
+    public string CategoryName { get; set; }
+    public string CategoryDescription { get; set; }
+    public bool Status { get; set; }
+}
---
+++ JadooTravel/Entities/TripPlan.cs
@@ -0,0 +1,10 @@
+namespace JadooTravel.Entities;
+
+public class TripPlan
+{
+    public string TripPlanId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string DescriptionIconUrl { get; set; }
+    
+}
